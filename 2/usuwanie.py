1ceadcf52b593505e0c5fbbf5bfda3354573210e
Kacper <kgrzesik17@gmail.com>
2024-05-07 22:07:12 +0200

    usuwanie.py pomyslnie usuwa pliki

--- 2/usuwanie.py
+++ 2/usuwanie.py
@@ -1,24 +1,51 @@
 import os
 
-def deletion(force=False):
+def deletion(prompt=True):
     """
     This function lets you delete all the files with user specified extension within a user specified path.
 
     Input:
-    force - skips the prompt if set to True.
+    prompt - skips the prompt if set to False.
 
     Output:
     boolean value - True if succeeded, False if failed.
     """
     path = str(input("Wybierz sciezke do folderu, z ktorego chcesz usunac pliki: ")) # ./wazne
-    extension = str(input("Wybierz rozszerzenie, dla ktorego chcesz usunac pliki: ")) # txt
+    extension = str(input("Wybierz rozszerzenie, dla ktorego chcesz usunac pliki: ")) # txt lub .txt
+
+    if extension[0] == ".": # also works if user adds a "." before the extension
+        extension = extension[1::]
 
     try:
-        dir = os.listdir(path)
+        files = os.listdir(path)
     except FileNotFoundError:
         print("Nie znaleziono folderu.")
         return False
+        
     
-    print(dir)
+    if prompt:
+        full_files = []
+
+        for file in files:
+            if file.endswith(f'.{extension}'):
+                full_files.append(file)
+
+        deletion = input(f'Czy jestes pewien, ze chcesz usunac te pliki: {full_files} [y/n]: ')
+        
+        if deletion == 'y':
+            for file in files:
+                if file.endswith(f".{extension}"):
+                    os.remove(os.path.join(path, file))
+
+        else:
+            print("Przerwano.")
+            return False
+        
+    else:
+        for file in files:
+                if file.endswith(f".{extension}"):
+                    os.remove(os.path.join(path, file))
+
+    print(f"Pomyslnie pliki o rozszerzeniu .{extension} ze sciezki {path}.")
 
 deletion()
